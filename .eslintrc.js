b652ea76c4ad413e8910e25249e3fe4cf79e7249
Rudolf Tucek <tucek.rudolf@gmail.com>
2019-12-22 03:09:07 +0100

    Relax `class-methods-use-this`
    By default his issues an error and complains at compile time.
    Using a warning is less annoying.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'class-methods-use-this': 'warn',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',

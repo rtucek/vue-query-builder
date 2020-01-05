4c6d7f3b9e3ced2ea59e99698539e374aa6c3e5a
Rudolf Tucek <tucek.rudolf@gmail.com>
2020-01-05 23:49:29 +0100

    Restructure for proper library
    Keep only library-relevant code in src/ directory.
    Move serve-related files for development to dev/
    Change build script for targeting library mode.

---
+++ dev/Number.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component({
+  model: {
+    event: 'input',
+    prop: 'value',
+  },
+})
+export default class Input extends Vue {
+  @Prop({
+    type: Number,
+    default: 0,
+  }) readonly value!: number;
+
+  get model() {
+    return this.value;
+  }
+
+  set model(value: any) {
+    if (typeof value === 'number') {
+      this.$emit('input', value);
+    }
+  }
+}
+</script>
+
+<template>
+  <input
+    type="number"
+    v-model.number="model"
+    placeholder="dummy number"
+  >
+</template>

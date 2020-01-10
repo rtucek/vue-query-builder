bc037c1ee838266b566611f90345d035854bd0dc
Rudolf Tucek <tucek.rudolf@gmail.com>
2020-01-10 23:33:15 +0100

    Inline SVG and CSS
    Inline assets so consumers don't have to manually import it.

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,18 @@
 module.exports = {
+  css: {
+    extract: false,
+  },
   chainWebpack: (config) => {
-    config.module.rule('eslint').use('eslint-loader').options({
-      fix: true,
-    });
+    config.module
+      .rule('eslint')
+      .use('eslint-loader')
+      .options({
+        fix: true,
+      });
+
+    config.module
+      .rule('vue')
+      .use('vue-svg-inline-loader')
+      .loader('vue-svg-inline-loader');
   },
 };

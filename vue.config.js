e4a50dd8acec53b2872ea3190d67f810bf1a5c96
Rudolf Tucek <tucek.rudolf@gmail.com>
2020-01-12 22:35:35 +0100

    Restore other config changes
    The last revert changed undid some other minor changes - here, we restore them.

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
 module.exports = {
+  css: {
+    extract: false,
+  },
   chainWebpack: (config) => {
-    config.module.rule('eslint').use('eslint-loader').options({
-      fix: true,
-    });
+    config.module
+      .rule('eslint')
+      .use('eslint-loader')
+      .options({
+        fix: true,
+      });
   },
 };

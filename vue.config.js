a687d74399900232a94ec8738079debaed5d5321
Rudolf Tucek <tucek.rudolf@gmail.com>
2020-01-13 00:50:44 +0100

    Override svg rule
    Use url-loader, rather than file-loader. This allows for inlining the svg file
    directly.

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,15 @@ module.exports = {
     extract: false,
   },
   chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule.use('url-loader')
+      .loader('url-loader')
+      .tap(options => ({
+        ...options,
+        limit: 10240,
+      }));
+
     config.module
       .rule('eslint')
       .use('eslint-loader')

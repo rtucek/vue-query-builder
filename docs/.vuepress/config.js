012b552e4bc8843917e3cf43ef7d53b1e38070f1
Rudolf Tucek <tucek.rudolf@gmail.com>
2020-01-30 22:38:50 +0100

    Bootstrap Vue Press and prepare document structure

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: 'Query Builder Vue',
+  description: 'Query Builder Vue documentation',
+
+  base: '/vue-query-builder/',
+
+  themeConfig: {
+      nav: [
+      ],
+
+      repo: 'rtucek/vue-query-builder',
+      docsDir: 'docs',
+      editLinks: true,
+
+      sidebar: [
+          ['/', 'Introduction'],
+          ['/demos', 'Demos'],
+          ['/getting-started', 'Getting Started'],
+          ['/configuration', 'Configuration'],
+          ['/styling', 'Styling'],
+          ['/contributing', 'Contributing'],
+      ],
+  },
+}

8711ccaf2355a18a0230dab86b6ac32d870e224a
Rudolf Tucek <tucek.rudolf@gmail.com>
2020-02-20 19:03:45 +0100

    Add Roboto font

--- dev/App.vue
+++ dev/App.vue
@@ -158,9 +158,13 @@ export default class App extends Vue {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
 body {
   margin: 0;
   padding: 0;
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
 }
 
 #app {
